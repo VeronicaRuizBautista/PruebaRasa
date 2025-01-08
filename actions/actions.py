9879df21b598c86f3c2b3eb50670c8e569101c30
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2025-01-08 15:28:14 -0500

    feat: :sparkles: Adding more options for default answer

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,29 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_out_of_scope")  # Usa el nombre existente
+
+        # Revertir el mensaje del usuario que llevó al fallback
+        return [UserUtteranceReverted()]
 
 # This is a simple example for a custom action which utters "Hello World!"
 
