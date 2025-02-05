32d0ba5a0f5789d901653bc8337b6f1f855980d8
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2025-02-05 11:03:00 -0500

    feat: :art: Update descarga.py

--- descarga.py
+++ descarga.py
@@ -1,5 +1,11 @@
+import os
 from sentence_transformers import SentenceTransformer
 
-model_name = "distilbert-base-nli-stsb-mean-tokens"
-model = SentenceTransformer(model_name)
-print(f"Modelo '{model_name}' cargado exitosamente.")
+model_name = "paraphrase-MiniLM-L6-v2"
+model_path = os.path.expanduser(f"~/.cache/torch/sentence_transformers/{model_name}")
+
+if not os.path.exists(model_path):
+    model = SentenceTransformer(model_name)
+    print(f"Modelo '{model_name}' descargado y guardado en {model_path}")
+else:
+    print(f"Modelo '{model_name}' ya existe en {model_path}")
